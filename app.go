f993fc146f4adc64ab426c4a4d9fa024cde84d60
Cleberson <pauluci.cleberson@gmail.com>
2024-02-11 21:08:23 -0300

    Added a pointer to code

--- app.go
+++ app.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 	age := 32 // regular variable
 
+	var agePointer *int // *int means a pointer of int type
+	agePointer = &age   // accessing the age memory address (pointer)
+	fmt.Println("Age pointer:", agePointer)
+
 	fmt.Println("Age:", age)
 
 	adultYears := getAdultYears(age)
