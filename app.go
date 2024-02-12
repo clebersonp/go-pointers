7b68e353c273cf7efe6b0d4e8c4209fcf84657df
Cleberson <pauluci.cleberson@gmail.com>
2024-02-11 21:29:36 -0300

    Create a function that receives a pointer and changed it directly inside the function

--- app.go
+++ app.go
@@ -15,9 +15,19 @@ func main() {
 
 	adultYears := getAdultYears(&age) // accessing the memory address and pass it as pointer
 	fmt.Println(adultYears)
+
+	fmt.Println("-----------------------")
+	fmt.Println("Example using pointer to mutate values directly from pointers")
+	fmt.Println("Age:", age)
+	editAgeToAdultYears(&age)
+	fmt.Println(age)
 }
 
 // age *int means that will pass a pointer (copy of address memory) not copy value
 func getAdultYears(age *int) int {
 	return *age - 18 //*age means to access the real data value from memory address
+}
+
+func editAgeToAdultYears(age *int) {
+	*age -= 18 // accessing the value from pointer and change it directly
 }
